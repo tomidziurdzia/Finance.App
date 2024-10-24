2fc354382622398652459c75a63c82f82cd476d9
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-10-24 20:45:09 +0200

    fix

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 import { ArrowDownIcon, ArrowUpIcon, WalletIcon } from "lucide-react";
