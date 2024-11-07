ae1ec1aecc99e85faaffa496eb2c705824a1e49a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-11-08 00:46:06 +0100

    rmeove

--- components/layout/index.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { TooltipProvider } from "../ui/tooltip";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function Layout({ children }: { children: any }) {
-  return <TooltipProvider delayDuration={500}>{children}</TooltipProvider>;
-}
